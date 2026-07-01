c13ad5122f0e4fe043c433309d3880d375d5ac34
Zo Kupelian <zkupelian@umass.edu>
2026-07-01 15:23:39 -0400

    Added scrolling interactivity

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # LymeX-Submission
+
+#git add, git commit -m "Comment Here", git push
+#in that order
--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-constole.log("Hello, this is from JavaScript!")
+const steps = document.querySelectorAll(".step");
+const viz = document.querySelector(".viz");
+
+function activateStep(stepNumber) {
+  viz.textContent = "Current step: " + stepNumber;
+
+  steps.forEach(step => {
+    step.classList.remove("active");
+  });
+
+  document
+    .querySelector(`[data-step="${stepNumber}"]`)
+    .classList.add("active");
+}
+
+window.addEventListener("scroll", () => {
+  let current = 1;
+
+  steps.forEach(step => {
+    const rect = step.getBoundingClientRect();
+
+    if (rect.top < window.innerHeight / 2) {
+      current = step.dataset.step;
+    }
+  });
+
+  activateStep(current);
+});
