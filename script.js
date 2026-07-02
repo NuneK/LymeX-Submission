6992e4b50f151580c96e588088a16716d3e22909
Zo Kupelian <zkupelian@umass.edu>
2026-07-02 12:49:56 -0400

    Minor script adjustment

--- script.js
+++ script.js
@@ -71,39 +71,21 @@ let hoveringGraphic = false;
 
 function showCircle() {
 
-    // Cancel any queued animations
     circle.interrupt();
     timelineGroup.interrupt();
 
-    timelineGroup
-        .transition()
-        .duration(300)
-        .style("opacity", 0);
-
-    circle
-        .style("opacity", 1)      // immediately restore visibility
-        .transition()
-        .duration(300)
-        .style("opacity", 1);
+    circle.style("opacity", 1);
+    timelineGroup.style("opacity", 0);
 
 }
 
 function showTimeline() {
 
-    // Cancel any queued animations
     circle.interrupt();
     timelineGroup.interrupt();
 
-    circle
-        .transition()
-        .duration(300)
-        .style("opacity", 0);
-
-    timelineGroup
-        .style("opacity", 1)
-        .transition()
-        .duration(300)
-        .style("opacity", 1);
+    circle.style("opacity", 0);
+    timelineGroup.style("opacity", 1);
 
     updateTimeline();
 
